eb42f2cb099a1c7a155ef056aa41afac39f87a28
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-04-12 01:44:07 +0530

    API

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,8 @@
-// module.exports = {
-//     MongoURI: 'mongodb+srv://shivanshu:shivanshu@1234@cluster0.jvgq6.mongodb.net/canabist?retryWrites=true&w=majority'
-// }
-
-module.exports = {
-    MongoURI: 'mongodb://localhost:27017/ejouma'
+ module.exports = {
+     MongoURI: 'mongodb+srv://shivanshu:shivanshu@1234@cluster0.t5khm.mongodb.net/ejouma?retryWrites=true&w=majority'
 }
 
-// Heroku Login: kinyadaf1@neighborhoodeals.com Password: Jennasmom-2001
+// module.exports = {
+//    MongoURI: 'mongodb://localhost:27017/ejouma'
+// }
 
-// FB
-// 437545507453292
-// 9e64cb70cee29dabd2d438e726266311
-// Mongo
-// kinyadaf1@neighborhoodeals.com
-// Jennasmom2001
